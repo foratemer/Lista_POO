f14feb36c298628d45bea1c1160e34f54db25b4b
arianne2016 <ariannesarmento.torcate@hotmail.com>
2016-09-25 23:31:36 -0300

    Add files via upload

---
+++ Exercicio_fatorial/fatorial.java
@@ -0,0 +1,37 @@
+package Poo;
+
+import java.util.Scanner;
+
+public class fatorial {
+
+	
+	public static void main (String [] args){
+		
+		Scanner input = new Scanner(System.in);
+
+		int fat = 1;
+		int numero = 0;
+		int cont = 1;
+		
+		
+		
+		System.out.println("Digite um número: ");
+		numero = input.nextInt();
+
+		
+		for( int i = 1; i <= numero; i++ ){
+			fat = fat * i;
+			{ 
+		}
+			
+		     
+		}
+		
+		System.out.println( "O fatorial de " + numero+ " é igual a " + fat );
+		cont++;
+
+			}
+		}
+	
+	
+	
